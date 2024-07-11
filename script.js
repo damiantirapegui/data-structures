55b63ab67fe40d9e0c3c27f03d4e8833546a6d5c
damiantirapegui <damiantirapegui@gmail.com>
2024-07-11 20:36:08 +0200

    Object destructure

--- script.js
+++ script.js
@@ -5,17 +5,6 @@
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // // Data needed for first part of the section
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-//   order: function (starterIndex, mainIndex) {
-//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//   },
-// };
 
 // const arr = [2, 3, 4];
 // const a = arr[0];
@@ -38,20 +27,6 @@
 
 // const [starter, mainCourse] = restaurant.order(2, 0);
 // // console.log(starter, mainCourse);
-// // openingHours: {
-// //   thu: {
-// //     open: 12,
-// //     close: 22,
-// //   },
-// //   fri: {
-// //     open: 11,
-// //     close: 23,
-// //   },
-// //   sat: {
-// //     open: 0, // Open 24 hours
-// //     close: 24,
-// //   },
-// // },
 
 // //Nested destructuring
 // const nested = [2, 4, [5, 6]];
@@ -62,6 +37,346 @@
 // // Default values
 // const [p = 1, q = 1, r = 1] = [8];
 // console.log(p, q, r);
+
+// const books = [
+//   {
+//     title: 'Algorithms',
+//     author: ['Robert Sedgewick', 'Kevin Wayne'],
+//     publisher: 'Addison-Wesley Professional',
+//     publicationDate: '2011-03-24',
+//     edition: 4,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'algorithms',
+//       'data structures',
+//       'java',
+//       'math',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 976,
+//     format: 'hardcover',
+//     ISBN: '9780321573513',
+//     language: 'English',
+//     programmingLanguage: 'Java',
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.41,
+//         ratingsCount: 1733,
+//         reviewsCount: 63,
+//         fiveStarRatingCount: 976,
+//         oneStarRatingCount: 13,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Structure and Interpretation of Computer Programs',
+//     author: [
+//       'Harold Abelson',
+//       'Gerald Jay Sussman',
+//       'Julie Sussman (Contributor)',
+//     ],
+//     publisher: 'The MIT Press',
+//     publicationDate: '2022-04-12',
+//     edition: 2,
+//     keywords: [
+//       'computer science',
+//       'programming',
+//       'javascript',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 640,
+//     format: 'paperback',
+//     ISBN: '9780262543231',
+//     language: 'English',
+//     programmingLanguage: 'JavaScript',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.36,
+//         ratingsCount: 14,
+//         reviewsCount: 3,
+//         fiveStarRatingCount: 8,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: "Computer Systems: A Programmer's Perspective",
+//     author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+//     publisher: 'Prentice Hall',
+//     publicationDate: '2002-01-01',
+//     edition: 1,
+//     keywords: [
+//       'computer science',
+//       'computer systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'engineering',
+//     ],
+//     pages: 978,
+//     format: 'hardcover',
+//     ISBN: '9780130340740',
+//     language: 'English',
+//     programmingLanguage: 'C',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.44,
+//         ratingsCount: 1010,
+//         reviewsCount: 57,
+//         fiveStarRatingCount: 638,
+//         oneStarRatingCount: 16,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'Operating System Concepts',
+//     author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+//     publisher: 'John Wiley & Sons',
+//     publicationDate: '2004-12-14',
+//     edition: 10,
+//     keywords: [
+//       'computer science',
+//       'operating systems',
+//       'programming',
+//       'software',
+//       'C',
+//       'Java',
+//       'engineering',
+//     ],
+//     pages: 921,
+//     format: 'hardcover',
+//     ISBN: '9780471694663',
+//     language: 'English',
+//     programmingLanguage: 'C, Java',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 3.9,
+//         ratingsCount: 2131,
+//         reviewsCount: 114,
+//         fiveStarRatingCount: 728,
+//         oneStarRatingCount: 65,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Engineering Mathematics',
+//     author: ['K.A. Stroud', 'Dexter J. Booth'],
+//     publisher: 'Palgrave',
+//     publicationDate: '2007-01-01',
+//     edition: 14,
+//     keywords: ['mathematics', 'engineering'],
+//     pages: 1288,
+//     format: 'paperback',
+//     ISBN: '9781403942463',
+//     language: 'English',
+//     programmingLanguage: null,
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.35,
+//         ratingsCount: 370,
+//         reviewsCount: 18,
+//         fiveStarRatingCount: 211,
+//         oneStarRatingCount: 6,
+//       },
+//     },
+//     highlighted: true,
+//   },
+//   {
+//     title: 'The Personal MBA: Master the Art of Business',
+//     author: 'Josh Kaufman',
+//     publisher: 'Portfolio',
+//     publicationDate: '2010-12-30',
+//     keywords: ['business'],
+//     pages: 416,
+//     format: 'hardcover',
+//     ISBN: '9781591843528',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.11,
+//         ratingsCount: 40119,
+//         reviewsCount: 1351,
+//         fiveStarRatingCount: 18033,
+//         oneStarRatingCount: 1090,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Crafting Interpreters',
+//     author: 'Robert Nystrom',
+//     publisher: 'Genever Benning',
+//     publicationDate: '2021-07-28',
+//     keywords: [
+//       'computer science',
+//       'compilers',
+//       'engineering',
+//       'interpreters',
+//       'software',
+//       'engineering',
+//     ],
+//     pages: 865,
+//     format: 'paperback',
+//     ISBN: '9780990582939',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.7,
+//         ratingsCount: 253,
+//         reviewsCount: 23,
+//         fiveStarRatingCount: 193,
+//         oneStarRatingCount: 0,
+//       },
+//     },
+//   },
+//   {
+//     title: 'Deep Work: Rules for Focused Success in a Distracted World',
+//     author: 'Cal Newport',
+//     publisher: 'Grand Central Publishing',
+//     publicationDate: '2016-01-05',
+//     edition: 1,
+//     keywords: ['work', 'focus', 'personal development', 'business'],
+//     pages: 296,
+//     format: 'hardcover',
+//     ISBN: '9781455586691',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.19,
+//         ratingsCount: 144584,
+//         reviewsCount: 11598,
+//         fiveStarRatingCount: 63405,
+//         oneStarRatingCount: 1808,
+//       },
+//     },
+//     highlighted: true,
+//   },
+// ];
+
+// //1. Destructure the books array into two variables called firstBook and secondBook.
+
+// //Solution:
+// const [firstBook, secondBook] = books;
+// // console.log(firstBook, secondBook);
+
+// //2. Destructure the books array into a variable called thirdBook. You must skip the first two books.
+
+// //Solution:
+// const [, , thirdBook] = books;
+// // console.log(thirdBook);
+
+// //3. Below is the nested ratings array that contains two other arrays.
+// //Destructure the nested ratings arrays into two variables called rating and ratingsCount.
+// //In the result of your destructuring, the ratings variable should store a number 4.19,
+// // and the ratingsCount variable should store a number 144584.
+
+// const ratings = [
+//   ['rating', 4.19],
+//   ['ratingsCount', 144584],
+// ];
+
+// // My Solution:
+// const [x, y] = ratings;
+// // Jonas Solution:
+// const [[, rating], [, ratingsCount]] = ratings;
+
+// console.log(typeof (rating, ratingsCount));
+
+// // Below is the ratingStars array. Destructure it into three variables called fiveStarRatings,
+// // oneStarRatings and threeStarRatings.
+// // Assign the threeStarRatings variable with a default value of 0.
+
+// const ratingStars = [63405, 1808];
+
+// // My Solution:
+// const [oneStarRatings, fiveStarRatings, threeStarRatings = 0] = ratingStars;
+// console.log(oneStarRatings, fiveStarRatings, threeStarRatings);
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    adress,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time} `
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: '22:30',
+  adress: 'via del sol, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+restaurant.orderDelivery({
+  adress: 'via del sol, 21',
+  starterIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant;
+// console.log(openingHours)
+
+//Renaming the variables
+const {
+  name: restaurantName,
+  openingHours: hours,
+  // categories: tags,
+} = restaurant;
+// console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested object
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
 
 const books = [
   {
@@ -288,41 +603,27 @@ const books = [
   },
 ];
 
-//1. Destructure the books array into two variables called firstBook and secondBook.
+// 1. Destructure the first book object from the books array into variables called title, author and ISBN.
+// My solution
+// const [{ title, ISBN, author }] = books; // Solution is good but you'll have to use many comas if you want last book
 
-//Solution:
-const [firstBook, secondBook] = books;
-// console.log(firstBook, secondBook);
+// Jonas solution
+// const { title, author, ISBN } = books[0]; // Good solution for bigger array with object. Choose index
+// console.log(title, ISBN, author);
 
-//2. Destructure the books array into a variable called thirdBook. You must skip the first two books.
+// 2. Each book object has the keywords property. Destructure the first book object from the books
+//  array into a variable called tags.
+// The tags variable should be assigned with the value of the keywords property.
 
-//Solution:
-const [, , thirdBook] = books;
-// console.log(thirdBook);
+// My solution
+// const { keywords: tags } = books[0];
+// console.log(tags);
 
-//3. Below is the nested ratings array that contains two other arrays.
-//Destructure the nested ratings arrays into two variables called rating and ratingsCount.
-//In the result of your destructuring, the ratings variable should store a number 4.19,
-// and the ratingsCount variable should store a number 144584.
+// 3. The seventh book from the books array is missing the programmingLanguage property.
+// Destructure the seventh book object (books[6]) into variables called language and programmingLanguage.
+// Assign the programmingLanguage variable with a default value of 'unknown'.
 
-const ratings = [
-  ['rating', 4.19],
-  ['ratingsCount', 144584],
-];
+// My solution
 
-// My Solution:
-const [x, y] = ratings;
-// Jonas Solution:
-const [[, rating], [, ratingsCount]] = ratings;
-
-console.log(typeof (rating, ratingsCount));
-
-// Below is the ratingStars array. Destructure it into three variables called fiveStarRatings,
-// oneStarRatings and threeStarRatings.
-// Assign the threeStarRatings variable with a default value of 0.
-
-const ratingStars = [63405, 1808];
-
-// My Solution:
-const [oneStarRatings, fiveStarRatings, threeStarRatings = 0] = ratingStars;
-console.log(oneStarRatings, fiveStarRatings, threeStarRatings);
+const { programmingLanguage = 'unknown', language } = books[6];
+console.log(programmingLanguage, language);
