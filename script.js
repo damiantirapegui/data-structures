cdd4b45402f86c485973d37696a0e89d5936b2dd
damiantirapegui <damiantirapegui@gmail.com>
2024-07-18 22:02:24 +0200

    assignment

--- script.js
+++ script.js
@@ -832,48 +832,132 @@ const books = [
 // };
 
 // Jonas solution
-function hasExamplesInJava(book) {
-  return book.programmingLanguage === 'Java' || 'no data available';
-}
+// function hasExamplesInJava(book) {
+//   return book.programmingLanguage === 'Java' || 'no data available';
+// }
 
-hasExamplesInJava(books[0]);
+// hasExamplesInJava(books[0]);
 
-for (let i = 0; i < books.length; i++) {
-  books[i].onlineContent ||
-    console.log(`"${books[i].title}" provides online content`);
-}
+// for (let i = 0; i < books.length; i++) {
+//   books[i].onlineContent ||
+//     console.log(`"${books[i].title}" provides online content`);
+// }
 
-const list = [
-  {
-    title: 'one',
-    value: true,
+// const list = [
+//   {
+//     title: 'one',
+//     value: true,
+//   },
+//   {
+//     title: 'two',
+//     value: false,
+//   },
+//   {
+//     title: 'three',
+//     value: false,
+//   },
+// ];
+
+// for (let i = 0; i < list.length; i++) {
+//   list[i].value || console.log(`is true ${list[i].title}`);
+//   // console.log([i]);
+// }
+
+// // || = värden som är sant, && falskt
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// // nullish: null and undefined (not 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+// for (let i = 0; i < books.length; i++) {
+//   books[i].onlineContent ??
+//     console.log(`${books[i].title} provides no online content`);
+// }
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+
+// GOOD LUCK 😀
+// */
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-  {
-    title: 'two',
-    value: false,
-  },
-  {
-    title: 'three',
-    value: false,
-  },
-];
+};
 
-for (let i = 0; i < list.length; i++) {
-  list[i].value || console.log(`is true ${list[i].title}`);
-  // console.log([i]);
+const players = game.scored;
+
+for (const value of players) [console.log(value)];
+
+// const odds = game.odds;
+
+// for (const value of odds) console.log(value);
+
+// odds = Object.values(game.odds);
+
+// for (const value of odds)
+const odds = game.odds;
+let sum = 0;
+let count = 0;
+
+for (let value of Object.values(odds)) {
+  sum += value;
+  count++;
+  console.log(sum);
 }
 
-// || = värden som är sant, && falskt
+const averageOdds = sum / count;
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
-
-// nullish: null and undefined (not 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
-
-for (let i = 0; i < books.length; i++) {
-  books[i].onlineContent ??
-    console.log(`${books[i].title} provides no online content`);
-}
+console.log(averageOdds);
